e7142cb276a359cffdfa70d11767e7f3a192ef02
‘CuiYanling’ <cyl_2379@163.com>
2023-02-18 17:18:27 +0800

    新生命周期

--- src/02-advance/20-新生命周期-1.js
+++ src/02-advance/20-新生命周期-1.js
@@ -1,29 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class App extends Component {
-    state  = {
-        myname:"kerwin",
-        myage:100
-    }
-    // componentWillMount 初始化
-    static getDerivedStateFromProps(nextProps,nextState){
-        // console.log(this)
-        console.log("getDrivedStateFromProps",nextState)
-        return {
-            myname:nextState.myname.substring(0,1).toUpperCase()+nextState.myname.substring(1)
-        }
-    }
+  state = {
+    myname: "cyl",
+    myage: 100,
+  };
 
-    render() {
-        return (
-            <div>
-                <button onClick={()=>{
-                    this.setState({
-                        myname:"xiaoming"
-                    })
-                }}>click</button>
-                app - {this.state.myname}-{this.state.myage}
-            </div>
-        )
-    }
+  static getDerivedStateFromProps(nextProps, nextState) {
+    // 代替 componentWillMount 初始化，但是后续更新时，还会运行
+    // console.log(this) //static静态 没有this
+    console.log("getDrivedStateFromProps", nextState);
+    return {
+      myname:
+        nextState.myname.substring(0, 1).toUpperCase() +
+        nextState.myname.substring(1),
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.setState({
+              myname: "xiaoming",
+            });
+          }}
+        >
+          click
+        </button>
+        app - {this.state.myname}-{this.state.myage}
+      </div>
+    );
+  }
 }
